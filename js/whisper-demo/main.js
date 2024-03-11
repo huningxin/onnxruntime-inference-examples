e14ed892e7c97f933cb3b47c7ad5f609f11e44bf
Lin <wanming.lin@intel.com>
2024-03-11 19:52:50 +0800

    Add UI

---
+++ js/whisper-demo/main.js
@@ -0,0 +1,227 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+// An example how to run whisper in onnxruntime-web.
+//
+
+import {log, Whisper} from './whisper.js';
+
+const kSampleRate = 16000;
+const kIntervalAudio_ms = 1000;
+const kSteps = kSampleRate * 30;
+const kDelay = 100;
+
+// ort session
+// let sess;
+// whisper class
+let whisper;
+
+let provider = 'webnn';
+
+// audio context
+var context = null;
+let mediaRecorder;
+
+// stats
+let total_processing_time = 0;
+let total_processing_count = 0;
+
+// some dom shortcuts
+let record;
+let transcribe;
+let progress;
+let audio_src;
+
+function getProvider() {
+    const query = window.location.search.substring('1');
+    const providers = ['webnn', 'webgpu', 'wasm'];
+    let vars = query.split('&');
+    for (let i = 0; i < vars.length; i++) {
+        let pair = vars[i].split('=');
+        if (pair[0] == 'provider' && providers.includes(pair[1])) {
+            return pair[1];
+        }
+    }
+    return provider;
+}
+
+// transcribe active
+function busy() {
+    transcribe.disabled = true;
+    progress.parentNode.style.display = "block";
+    document.getElementById("outputText").value = "";
+    document.getElementById('latency').innerText = "";
+}
+
+// transcribe done
+function ready() {
+    transcribe.disabled = false;
+    progress.style.width = "0%";
+    progress.parentNode.style.display = "none";
+}
+
+// called when document is loaded
+document.addEventListener("DOMContentLoaded", async () => {
+    audio_src = document.querySelector('audio');
+    record = document.getElementById('record');
+    transcribe = document.getElementById('transcribe');
+    progress = document.getElementById('progress');
+    transcribe.disabled = true;
+    progress.parentNode.style.display = "none";
+
+    // audio_src.src = "colorado_river_crisis_is_hitting_home.mp3";
+
+    // click on Record
+    record.addEventListener("click", (e) => {
+        if (e.currentTarget.innerText == "Record") {
+            e.currentTarget.innerText = "Stop Recording";
+            startRecord(0);
+        }
+        else {
+            e.currentTarget.innerText = "Record";
+            stopRecord();
+        }
+    });
+
+    // click on Transcribe
+    transcribe.addEventListener("click", () => {
+        transcribe_file();
+    });
+
+    // drop file
+    document.getElementById("file-upload").onchange = function (evt) {
+        let target = evt.target || window.event.src, files = target.files;
+        audio_src.src = URL.createObjectURL(files[0]);
+    }
+
+    log("loading model");
+    try {
+        whisper = new Whisper('https://huggingface.co/lwanming/whisper-base-static-shape/resolve/main/', getProvider());
+        await whisper.create_whisper_processor();
+        await whisper.create_whisper_tokenizer();
+        await whisper.create_ort_sessions();
+        ready();
+        context = new AudioContext({
+            sampleRate: kSampleRate,
+            channelCount: 1,
+            echoCancellation: false,
+            autoGainControl: true,
+            noiseSuppression: true,
+        });
+        if (!context) {
+            throw new Error("no AudioContext, make sure domain has access to Microphone");
+        }
+    } catch (e) {
+        log(`Error: ${e}`);
+    }
+});
+
+// report progress
+function update_status(t) {
+    total_processing_time += t;
+    total_processing_count += 1;
+    const avg = 1000 * 30 * total_processing_count / total_processing_time;
+    document.getElementById('latency').innerText = `${avg.toFixed(1)} x realtime`;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// process audio buffer
+async function process_audio(audio, starttime, idx, pos) {
+    if (idx < audio.length) {
+        // not done
+        try {
+            // update progress bar
+            progress.style.width = (idx * 100 / audio.length).toFixed(1) + "%";
+            progress.textContent = progress.style.width;
+            await sleep(kDelay);
+
+            // run inference for 30 sec
+            const xa = audio.slice(idx, idx + kSteps);
+            const start = performance.now();
+            const ret = await whisper.run(xa, kSampleRate);
+            const diff = performance.now() - start;
+            update_status(diff);
+
+            // append results to textarea 
+            const textarea = document.getElementById('outputText');
+            textarea.value += ret; // `${ret.str.data[0]}\n`;
+            textarea.scrollTop = textarea.scrollHeight;
+            await sleep(kDelay);
+            process_audio(audio, starttime, idx + kSteps, pos + 30);
+        } catch (e) {
+            log(`Error: ${e}`);
+            ready();
+        }
+    } else {
+        // done with audio buffer
+        const processing_time = ((performance.now() - starttime) / 1000);
+        const total = (audio.length / kSampleRate);
+        log(`${document.getElementById('latency').innerText}, total ${processing_time.toFixed(1)}sec for ${total.toFixed(1)}sec`);
+        ready();
+    }
+}
+
+// transcribe audio source
+async function transcribe_file() {
+    if (audio_src.src == "") {
+        log("Error: set some Audio input");
+        return;
+    }
+
+    busy();
+    log("start transcribe ...");
+    try {
+        const buffer = await (await fetch(audio_src.src)).arrayBuffer();
+        const audioBuffer = await context.decodeAudioData(buffer);
+        var offlineContext = new OfflineAudioContext(audioBuffer.numberOfChannels, audioBuffer.length, audioBuffer.sampleRate);
+        var source = offlineContext.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(offlineContext.destination);
+        source.start();
+        const renderedBuffer = await offlineContext.startRendering();
+        const audio = renderedBuffer.getChannelData(0);
+        process_audio(audio, performance.now(), 0, 0);
+    }
+    catch (e) {
+        log(`Error: ${e}`);
+        ready();
+    }
+}
+
+// start recording
+async function startRecord() {
+    if (mediaRecorder === undefined) {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+            mediaRecorder = new MediaRecorder(stream);
+        } catch (e) {
+            record.innerText = "Record";
+            log(`Access to Microphone, ${e}`);
+        }
+    }
+    let recording_start = performance.now();
+    let chunks = [];
+
+    mediaRecorder.ondataavailable = (e) => {
+        chunks.push(e.data);
+        document.getElementById('latency').innerText = `recorded: ${((performance.now() - recording_start) / 1000).toFixed(1)}sec`;
+    }
+
+    mediaRecorder.onstop = () => {
+        const blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
+        log(`recorded ${((performance.now() - recording_start) / 1000).toFixed(1)}sec audio`);
+        audio_src.src = window.URL.createObjectURL(blob);
+    };
+    mediaRecorder.start(kIntervalAudio_ms);
+}
+
+// stop recording
+function stopRecord() {
+    if (mediaRecorder) {
+        mediaRecorder.stop();
+        mediaRecorder = undefined;
+    }
+}
--- js/whisper-demo/whisper.js
+++ js/whisper-demo/whisper.js
@@ -4,64 +4,72 @@ import { AutoProcessor, AutoTokenizer, read_audio } from 'https://cdn.jsdelivr.n
 import { get_new_tokens } from './generation_utils.js';
 import { attention_mask_update, cache_update } from './post_processing.js';
 
-ort.env.wasm.simd = true;
-
-let encoder_session = null;
-let decoder_session = null;
-let decoder_cached_session = null;
-
-const max_sequence_length = 128;
-// No. of tokens to be used for decoder 1st inference
-const num_init_tokens = 4;
-// Whisper was trained using 16000 Hz as sampling rate, fixing this value for dataset preparation
-const sampling_rate = 16000;
-// set precision of different inputs
-const cache_precision = Float32Array;
-let processor = null;
-let tokenizer = null;
-
-function log(i) {
+export function log(i) { 
     console.log(i);
-    // document.getElementById("status").innerText += `\n[${performance.now().toFixed(3)}] ` + i;
+    document.getElementById('status').innerText += `\n[${performance.now().toFixed(2)}] ` + i;
 }
 
-async function create_whisper_processor() {
+// wrapper around onnxruntime and model
+export class Whisper {
+    constructor(url, provider) {
+        this.url = url;
+        this.provider = provider;
+        ort.env.wasm.simd = true;
+
+        this.models = {
+            'encoder': { url: 'whisper_base_encoder.onnx', sess: null },
+            'decoder': { url: 'whisper_base_decoder_static_non_kvcache_logits.onnx', sess: null },
+            'decoder_cached': { url: 'whisper_base_decoder_static_kvcache_logits.onnx', sess: null },
+        };
+
+        this.max_sequence_length = 128;
+        // No. of tokens to be used for decoder 1st inference
+        this.num_init_tokens = 4;
+        // Whisper was trained using 16000 Hz as sampling rate, fixing this value for dataset preparation
+        this.sampling_rate = 16000;
+        // set precision of different inputs
+        this.cache_precision = Float32Array;
+        this.processor = null;
+        this.tokenizer = null;
+    }
+
+async create_whisper_processor() {
     // processor contains feature extractor
-    processor = await AutoProcessor.from_pretrained('openai/whisper-tiny.en');
+    this.processor = await AutoProcessor.from_pretrained('openai/whisper-tiny.en');
 }
 
-async function create_whisper_tokenizer() {
+async create_whisper_tokenizer() {
     // processor contains feature extractor
-    tokenizer = await AutoTokenizer.from_pretrained('openai/whisper-tiny.en', { config: { do_normalize: true } });
+    this.tokenizer = await AutoTokenizer.from_pretrained('openai/whisper-tiny.en', { config: { do_normalize: true } });
 }
 
-async function create_ort_sessions() {
+async create_ort_sessions() {
     const options = {
         executionProviders: [{
-            name: 'wasm',
+            name: this.provider,
             deviceType: 'gpu',
         }],
     };
     // options.logSeverityLevel = 0;
-    const encoder_path = 'https://huggingface.co/lwanming/whisper-base-static-shape/resolve/main/whisper_base_encoder.onnx';
-    const decoder_path = 'https://huggingface.co/lwanming/whisper-base-static-shape/resolve/main/whisper_base_decoder_static_non_kvcache_logits.onnx';
-    const decoder_cached_path = 'https://huggingface.co/lwanming/whisper-base-static-shape/resolve/main/whisper_base_decoder_static_kvcache_logits.onnx';
-    encoder_session = await ort.InferenceSession.create(encoder_path, options);
-    log("encoder session created...");
-    decoder_session = await ort.InferenceSession.create(decoder_path, options);
-    log("decoder session created...");
-    decoder_cached_session = await ort.InferenceSession.create(decoder_cached_path, options);
-    log("decoder with cache session created...");
+    for (let name of Object.keys(this.models)) {
+        try {
+            this.models[name]['sess'] = await ort.InferenceSession.create(this.url + this.models[name]['url'], options);
+            log(`Model ${this.models[name]['url']} loaded`);
+        } catch (e) {
+            log(`Error: ${e}`);
+        }
+    };
 }
 
-async function run_session() {
+async run(audio_data, sampling_rate) {
     // -----------------------------------FEATURE EXTRACTION-----------------------------------------
-    const audio = await read_audio('https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac', 16000);
-    const { input_features } = await processor(audio);
+    // const audio = await read_audio('https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac', 16000);
+    // const audio = await read_audio(audio_data, sampling_rate);
+    const { input_features } = await this.processor(audio_data);
 
     // -----------------------------------ENCODER INFERENCE-----------------------------------------
     // run encoder to get output
-    const encoder_hidden_states = await encoder_session.run({
+    const encoder_hidden_states = await this.models['encoder']['sess'].run({
         input_features: new ort.Tensor(input_features.type, input_features.data, input_features.dims)
     });
 
@@ -77,7 +85,7 @@ async function run_session() {
         'encoder_hidden_states': encoder_hidden_states['last_hidden_state'],
     };
     // run the first inference which generates SA and CA KV cache
-    const decoder_output = await decoder_session.run(decoder_input);
+    const decoder_output = await this.models['decoder']['sess'].run(decoder_input);
 
     const logits = decoder_output['logits']['cpuData'];
     // find out the token with highest probability, cast INT64 to INT32
@@ -97,9 +105,9 @@ async function run_session() {
 
     // pad attention mask to max_seq_length
     decoder_input['attention_mask'] = new ort.Tensor('int64', attention_mask_update(new BigInt64Array([1n, 1n, 1n, 1n]),
-        0, max_sequence_length, num_init_tokens), [1, 128]); // TODO try to update cpuData directly
+        0, this.max_sequence_length, this.num_init_tokens), [1, 128]);
     // create past_key_values_length as input, value should be same of No. of prefill tokens
-    decoder_input['past_key_values_length'] = new ort.Tensor('int32', new Int32Array([num_init_tokens]), [1]);
+    decoder_input['past_key_values_length'] = new ort.Tensor('int32', new Int32Array([this.num_init_tokens]), [1]);
 
     // fill decoder kv cache model inputs with cross attention KV cache data from decoder 1st inference
     for (let i = 0; i < 6; i++) {
@@ -112,13 +120,13 @@ async function run_session() {
         decoder_input,
         decoder_output,
         0,
-        max_sequence_length,
-        num_init_tokens,
-        cache_precision);
+        this.max_sequence_length,
+        this.num_init_tokens,
+        this.cache_precision);
     const past_key_values_length = new Int32Array(decoder_input['past_key_values_length'].cpuData.buffer);
     // run complete inference for every item in dataset
-    for (let i = 1; i < max_sequence_length; i++) {
-        const decoder_cached_output = await decoder_cached_session.run(decoder_input);
+    for (let i = 4; i < this.max_sequence_length; i++) {
+        const decoder_cached_output = await this.models['decoder_cached']['sess'].run(decoder_input);
 
         // find out the token with highest probability, cast INT64 to INT32
         const new_token = get_new_tokens(decoder_cached_output['logits']['cpuData'], [1, 1, 51865]);
@@ -134,9 +142,7 @@ async function run_session() {
         past_key_values_length[0] = past_key_values_length[0] + 1;
 
         // update mask using position id
-        decoder_input['attention_mask'] = new ort.Tensor('int64',
-            attention_mask_update(decoder_input['attention_mask'].cpuData, i, past_key_values_length[0]), [1, 128]);
-
+        attention_mask_update(decoder_input['attention_mask'].cpuData, i, this.max_sequence_length, this.num_init_tokens, past_key_values_length[0]);
         // modify the kv cache in place
         cache_update(decoder_input, decoder_cached_output, i, past_key_values_length[0]);
         // break if the new token is eos_token_id: 50256 (end of sequence)
@@ -146,7 +152,7 @@ async function run_session() {
     }
 
     // add token to sentence decode time
-    const sentence = await tokenizer.decode(tokens, { skip_special_tokens: true });
+    const sentence = await this.tokenizer.decode(tokens, { skip_special_tokens: true });
 
     // TODO
     // norm_ref = self.processor.tokenizer._normalize(input_data['reference']);
@@ -155,12 +161,14 @@ async function run_session() {
     console.log('result: ', sentence);
     return sentence;
 }
-
-async function main() {
-    await create_whisper_processor();
-    await create_whisper_tokenizer();
-    await create_ort_sessions();
-    await run_session();
 }
 
-main();
+async function main() {
+    const whisper = new Whisper('./models/');
+    await whisper.create_whisper_processor();
+    await whisper.create_whisper_tokenizer();
+    await whisper.create_ort_sessions();
+    await whisper.run();
+}
+
+// main();
